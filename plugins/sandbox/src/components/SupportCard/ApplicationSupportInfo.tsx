816625f921d4aaa86568a0e8c2d246f6bdffae2d
Brandon Bryant <brandon@Brandons-iMac.local>
2024-12-06 16:02:06 -0500

    about field

--- plugins/sandbox/src/components/SupportCard/ApplicationSupportInfo.tsx
+++ plugins/sandbox/src/components/SupportCard/ApplicationSupportInfo.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ApplicationInfo } from './EntitySupportCard';
-import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import { MarkdownContent } from '@backstage/core-components';
+import { AboutField } from '@backstage/plugin-catalog';
+import Typography from '@material-ui/core/Typography';
 
 interface ApplicationSupportInfoProps {
   applicationInfo: ApplicationInfo;
@@ -11,38 +11,6 @@ interface ApplicationSupportInfoProps {
 }
 
 export const ApplicationSupportInfo = ({ applicationInfo, supportInfo }: ApplicationSupportInfoProps) => {
-  const InfoCell = ({ title, value }: { title: string, value: string }) => (
-    <Grid item xs={12} sm={6} md={4}>
-      <Box mb={1}>
-        <Typography variant="subtitle2" color="textSecondary">
-          {title}
-        </Typography>
-        <Typography variant="body1">
-          <strong>{value}</strong>
-        </Typography>
-      </Box>
-    </Grid>
-  );
-
-  const ManagersCell = () => (
-    <Grid item xs={12} sm={6} md={4}>
-      <Box mb={1}>
-        <Typography variant="subtitle2" color="textSecondary">
-          MANAGERS
-        </Typography>
-        <Typography variant="body1">
-          <strong>Development:</strong> {applicationInfo.appDevManager}
-        </Typography>
-        <Typography variant="body1">
-          <strong>IT Prod. Manager:</strong> {applicationInfo.itProductManager}
-        </Typography>
-        <Typography variant="body1">
-          <strong>Bus. Prod. Manager:</strong> {applicationInfo.businessProductManager}
-        </Typography>
-      </Box>
-    </Grid>
-  );
-
   return (
     <>
       {supportInfo && (
@@ -51,12 +19,35 @@ export const ApplicationSupportInfo = ({ applicationInfo, supportInfo }: Applica
         </Grid>
       )}
       <Grid container spacing={3}>
-        <InfoCell title="APP ID" value={applicationInfo.appId} />
-        <InfoCell title="APP NAME" value={applicationInfo.appName} />
-        <ManagersCell />
-        <InfoCell title="CRITICALITY CODE" value={applicationInfo.criticalityCode} />
-        <InfoCell title="RBF RATING" value={applicationInfo.rbfGovernedRating} />
-        <InfoCell title="SECURITY TIER" value={applicationInfo.securityLevel} />
+        <AboutField label="App ID" value={applicationInfo.appId} gridSizes={{ xs: 12, sm: 6, md: 4 }} />
+        <AboutField label="App Name" value={applicationInfo.appName} gridSizes={{ xs: 12, sm: 6, md: 4 }} />
+        
+        <AboutField 
+          label="Managers" 
+          gridSizes={{ xs: 12, sm: 6, md: 4 }}
+        >
+          <Typography variant="body2">
+            <strong>Development:</strong> {applicationInfo.appDevManager}<br />
+            <strong>IT Prod. Manager:</strong> {applicationInfo.itProductManager}<br />
+            <strong>Bus. Prod. Manager:</strong> {applicationInfo.businessProductManager}
+          </Typography>
+        </AboutField>
+
+        <AboutField 
+          label="Criticality Code" 
+          value={applicationInfo.criticalityCode} 
+          gridSizes={{ xs: 12, sm: 6, md: 4 }} 
+        />
+        <AboutField 
+          label="RBF Rating" 
+          value={applicationInfo.rbfGovernedRating} 
+          gridSizes={{ xs: 12, sm: 6, md: 4 }} 
+        />
+        <AboutField 
+          label="Security Tier" 
+          value={applicationInfo.securityLevel} 
+          gridSizes={{ xs: 12, sm: 6, md: 4 }} 
+        />
       </Grid>
     </>
   );
